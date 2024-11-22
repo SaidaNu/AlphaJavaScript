1c67af56602878aca69e92201e4968ddc8da202c
Saida Nurlanova <saidanurlanova01@gmail.com>
2024-11-22 22:08:48 +0500

    lecture 2

--- lecture_1/script.js
+++ lecture_1/script.js
@@ -1,1 +1,53 @@
 console.log("Hello, World!");
+// console.log(typeof 27);
+
+// const lang = "JavaScript";
+// let year = 1995;
+// year = 2024;
+// let isAuth = false;
+// console.log(lang);
+// console.log(year, typeof year);
+// console.log(isAuth, typeof isAuth);
+
+// const year = 1995;
+// const newYear = 2024;
+// console.log(newYear.toString());
+// console.log(typeof newYear);
+
+// let age = "35";
+// age = Number(age); // or age=+age;
+// console.log(age);
+// console.log(typeof age);
+
+// let isAuth = false;
+// isAuth = "" + isAuth;  Когда false - "", 0
+// console.log(isAuth);   Когда true - всё остальное
+// console.log(typeof isAuth);
+
+// const year = 1995;
+// const message = "JavaScript был создан в " + year + " году";
+// console.log(message);
+
+// const fileName = "data.csv";
+// const fileSize = 2048;
+// const message =
+//   "Файл " + fileName + " успешно загружен. Размер файла " + fileSize + " кБ.";
+// console.log(message);
+
+// const fileName = "data.csv";
+// const fileSize = 2048;
+// const message = `Файл ${fileName} успешно загружен. Размер файла ${fileSize} кБ.`;
+// console.log(message);
+
+// const fileName = "data.csv";
+// const fileSize = 2048;
+// const firstLineOfMessage = "Файл " + fileName + " успешно загружен.";
+// const secondLineOfMessage = "Размер файла " + fileSize + " кБ.";
+// console.log(firstLineOfMessage);
+// console.log(secondLineOfMessage);
+
+const fileName = "data.csv";
+const fileSize = 2048;
+const message = `Файл ${fileName} успешно загружен.
+Размер файла ${fileSize} кБ.`;
+console.log(message);
--- lecture_2/script.js
+++
@@ -1,4 +0,0 @@
-const lang = "JavaScript";
-const year = 1995;
-console.log(lang);
-console.log(year);
